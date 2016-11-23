b7df17efccbead7630c6320caa9023bdfa484a0c
Bill Canfield <wdc.rith@gmail.com>
2016-11-23 12:10:38 -0500

    Actually adding changes to task_scheduler.py as mentioned in previous commit cdce9c4df6cd0a35e655bbd79080e0c7c5484708

--- task_scheduler/task_scheduler.py
+++ task_scheduler/task_scheduler.py
@@ -1,5 +1,12 @@
-import yaml
-import copy
+import sys
+
+def check_python3():
+    if sys.version_info < (3, 0):
+        sys.stdout.write('\nRequires Python3, ABORTING\n')
+        sys.exit(1)
+
+if __name__ == 'task_scheduler':
+    check_python3();
 
 class TaskScheduler:
     def __init__(self):
@@ -13,19 +20,20 @@ class TaskScheduler:
             self.execution_time = execution_time
             self.parents = []
             self.children = []
-            self.parents_done = False
 
         def add_parent(self, parent):
-            if parent:
+            if isinstance(parent, self.__class__):
                 self.parents.append(parent)
 
         def add_child(self, child):
-            if child:
+            if isinstance(child, self.__class__):
                 self.children.append(child)
 
         def get_full_execution_time(self, exclude_parents=[]):
             full_execution_time = self.execution_time
             # We want to ignore parents that overlap between dependent tasks
+            # due to including them leading to multiples of the same execution times
+            # being includes in the full execution times
             for p in list(set(self.parents) - set(exclude_parents)):
                 full_execution_time += p.get_full_execution_time(exclude_parents+self.parents)
             return full_execution_time
@@ -34,12 +42,9 @@ class TaskScheduler:
             return self.execution_time <= 0
 
         def is_parents_done(self):
-            if not self.parents_done:
-                if len(self.parents):
-                    self.parents_done = all(map(lambda p: p.is_done(), self.parents))
-                else:
-                    self.parents_done = True
-            return self.parents_done
+            if len(self.parents):
+                return all(map(lambda p: p.is_done(), self.parents))
+            return True
 
         def is_ready(self):
             return not self.is_done() and self.is_parents_done()
@@ -53,28 +58,33 @@ class TaskScheduler:
         def __init__(self, name, cores_total):
             self.name = name
             self.cores_total = cores_total
-            self.cores_in_use = 0
+            self.cores_used = 0
             self.tasks_in_progress = []
 
         def task_count(self):
             return len(self.tasks_in_progress)
 
+        def cores_available(self):
+            return self.cores_total - self.cores_used
+
         def add_task(self, task):
             if task.cores_required <= self.cores_available():
                 self.tasks_in_progress.append(task)
+                #Print out task name and what compute resource it's on
                 print(task.name + ': ' + self.name)
-                self.cores_in_use += task.cores_required
+                self.cores_used += task.cores_required
                 return True
             return False
 
         def remove_task(self, task):
-            self.tasks_in_progress.remove(task)
-            self.cores_in_use -= task.cores_required
-
-        def cores_available(self):
-            return self.cores_total - self.cores_in_use
+            try:
+                self.tasks_in_progress.remove(task)
+                self.cores_used -= task.cores_required
+            except ValueError:
+                pass # If the task doesn't exist in our list ignore it
 
         def iterate(self):
+            """ Iterate all tasks in progress removing tasks once done """
             task_completed = False
             for task in self.tasks_in_progress:
                 task.iterate()
@@ -85,6 +95,7 @@ class TaskScheduler:
 
 
     def prioritize_tasks(self):
+        """ Prioritize tasks based on execution time and core count """
         def get_priority_metric(name):
             # Our priority metric is the full execution time of a task
             # (execution time of the task plus all ancestor tasks execution times
@@ -95,20 +106,44 @@ class TaskScheduler:
         prioritized_tasks = sorted(self.tasks, key=get_priority_metric, reverse=True)
         return prioritized_tasks
 
-    def find_free_compute_resource(self, task, resources):
-        for resource in sorted(resources, key=lambda r: r.cores_available()):
+    def find_free_compute_resource(self, task):
+        """ Find the compute resource based on cores available non-greedy """
+        for resource in sorted(self.resources, key=lambda r: r.cores_available()):
             if resource.cores_available() >= task.cores_required:
                 return resource
         return None
 
     def load_yaml(self, resources_path, tasks_path):
-        resource_dict = yaml.safe_load(open(resources_path, 'r'))
-        task_dict = yaml.safe_load(open(tasks_path, 'r'))
+        """ Load resources and task yaml description files and
+            initialize classes for each """
+        # Attempt to use the system's pyyaml, if that fails use our own
+        try:
+            import yaml
+        except ImportError:
+            sys.path.insert(0, 'pyyaml')
+            import yaml
+
+        # Try and load yaml files in separate try blocks so we can error our
+        # more descriptively
+        try:
+            resource_dict = yaml.safe_load(open(resources_path, 'r'))
+        except:
+            print('\nERROR: "{0}", failed to load\n'.format(resources_path))
+            exit(-1)
+        try:
+            task_dict = yaml.safe_load(open(tasks_path, 'r'))
+        except:
+            print('\nERROR: "{0}", failed to load\n'.format(tasks_path))
+            exit(-1)
 
         # Initialize task objects from dict
         for name, value in task_dict.items():
-            self.tasks[name] = self.Task(name, value['cores_required'],
-                value['execution_time'])
+            try:
+                self.tasks[name] = self.Task(name, value['cores_required'],
+                    value['execution_time'])
+            except KeyError:
+                print('\nERROR: task "{0}", expected keys in load tasks yaml are not where expected\n'.format(name))
+                exit(-1)
 
         # Link parents<->children
         for name, value in task_dict.items():
@@ -119,48 +154,64 @@ class TaskScheduler:
                         self.tasks[name].add_parent(parent)
                         parent.add_child(self.tasks[name])
                     else:
-                        print('ERROR: {0}\'s parent task: "{1}", does not exist in task list.'.format(name, pname))
+                        print('\nERROR: {0}\'s parent task: "{1}", does not exist in task list\n'.format(name, pname))
                         exit(-1)
 
         # Initialize sorted (by name) compute resources list
         for name, cores in sorted(resource_dict.items(), key=lambda r: r[0]):
+            if not isinstance(cores, int):
+                print('\nERROR: resource "{0}" cores count is not an interger value\n'.format(name))
+                exit(-1)
             self.resources.append(self.ComputeResource(name, cores))
 
-    def has_circular_dependencies(self):
-        def traverse_children(taskname, children, entry=False):
-            for child in children:
-                if taskname == child.name:
-                    return True
-                return traverse_children(taskname, child.children)
-            return False
-
+    def check_circular_dependencies(self):
+        """ Simple circular build dependency check by counting depedents
+            seen and comparing to total task count"""
         for name, task in self.tasks.items():
-            if traverse_children(task.name, task.children, entry=True):
-                return True
+            import copy
+            dependents = copy.copy(task.children)
+            for idx, depend in enumerate(dependents):
+                if idx > len(self.tasks.keys()):
+                    print('\nERROR: Circular dependencies detected between tasks\n')
+                    exit(-1)
+                dependents += depend.children
+
+    def check_resources_needed(self):
+        """ Verify that all given tasks are able to fit on a resource """
+        for name, task in self.tasks.items():
+            if self.find_free_compute_resource(task) is None:
+                print('\nERROR: Task "{0}" requires {1} cores, no resoure can handle that requirement\n'.format(task.name, task.cores_required))
+                exit(-1)
 
     def find_schedule(self):
-        if self.has_circular_dependencies():
-            print("ERROR: Circular dependencies detected between tasks")
-            exit(-1)
+        """ Simulator for finding the optimum task schedule """
+
+        # Check for circular dependencies and tasks that are oversized for our
+        # resources, erroring our and exiting if so
+        self.check_circular_dependencies()
+        self.check_resources_needed()
+
         current_ticks = 0 
         prioritized_tasks = self.prioritize_tasks()
-        print(prioritized_tasks)
+        print('PRIORITIZED_TASKS: ' + str(prioritized_tasks) + '\n')
         while prioritized_tasks:
             tasks_processed = False
-            # For each prioritized task try and assign it to a free compute resource
+            # For each prioritized task try and assign 
+            # it to a free compute resource
             for t in prioritized_tasks:
                 task = self.tasks[t]
                 if task.is_ready():
-                    res = self.find_free_compute_resource(task, self.resources)
-                    #if we
-                    if res is None:
-                        continue
-                    print('-'*40 + str(current_ticks) + '-'*40)
-                    if res.add_task(task):
-                        prioritized_tasks.remove(task.name)
-                        tasks_processed = True
-                    else:
-                        print("failed to add task to resource")
+                    res = self.find_free_compute_resource(task)
+                    # If we found a free resource let's add the task to it
+                    if res:
+                        # Uncomment line for testing to see at what ticks tasks
+                        # were added
+                        #print('-'*40 + str(current_ticks) + '-'*40)
+                        if res.add_task(task):
+                            prioritized_tasks.remove(task.name)
+                            tasks_processed = True
+                        else:
+                            print("Failed to add task to resource")
             for resource in self.resources:
                 resource.iterate()
             current_ticks += 1
@@ -170,10 +221,23 @@ class TaskScheduler:
             current_ticks += 1
             for resource in self.resources:
                 resource.iterate()
-        print("MAKESPAN: {0}".format(current_ticks))
+        # If we care to double check the makespan, uncomment and run
+        print("\nSCHEDULE MAKESPAN: {0}".format(current_ticks))
         
 
 if __name__ == '__main__':
+    check_python3()
+    import argparse
+
+    argparser = argparse.ArgumentParser(
+        description='Generates task schedule based on a task list' +
+                    ' and compute resources available')
+
+    argparser.add_argument('task_yaml', type=str, help='task description yaml file')
+    argparser.add_argument('resource_yaml', type=str, help='resource description yaml file')
+
+    args = argparser.parse_args()
+
     ts = TaskScheduler()
-    ts.load_yaml('resources.yaml', 'tasks.yaml')
+    ts.load_yaml(args.resource_yaml, args.task_yaml)
     ts.find_schedule()
